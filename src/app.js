a57f9c398f845c08e3e5cf09e5bcc046d9e6e02e
Roberta Capalbo <robertacapalbo@hotmail.com>
2023-04-17 11:58:36 -0300

    ajuste

--- src/app.js
+++ src/app.js
@@ -67,7 +67,7 @@ app.post("/messages", async (req, res) => {
         const exstPt = await db.collection("participants").findOne({ name: user })
         if (exstPt === null) return res.sendStatus(422)
         const time = dayjs().format("HH:mm:ss")
-        const newMessage = { from: user, to: to, text: text, type: type, time: time }
+        const newMessage = { from: user, to, text, type, time }
         await db.collection("messages").insertOne(newMessage)
         return res.sendStatus(201)
     } catch (err) {
